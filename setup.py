b42262a65a5f2a6313c9b01e862843aa59924c5c
Naomi Ohashi <ohashin2G@gmail.com>
2023-03-28 23:08:23 -0400

    Replace with setupfile

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A simple demonstration package',
+      url=https://github.com/ohashin2G/DS5100_Project,
+      author='N. Ohashi',
+      author_email='fju4ek@vierginia.edu',
+      license='MIT',
+      packages=['m09_helloworld.py'])
